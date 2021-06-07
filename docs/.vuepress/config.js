4b8593d99fff3b1142883dd2a55dd79d2d1b77c3
Vinicius Fagundes <vinicius_fagundes@epam.com>
2021-06-07 06:49:03 +0200

    aqadocs - adding navigation bar items

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,21 +1,52 @@
 module.exports = {
     head: [
-        ['link', { rel: 'icon', href: '/assets/logo.png' }]
-      ],
+        ['link', {
+            rel: 'icon',
+            href: '/assets/logo.png'
+        }]
+    ],
     title: 'alavanQA Docs',
     description: 'Documentation Repository for alavanQA material',
     themeConfig: {
         logo: '/assets/logo.png',
-        sidebar: {
-            "/guide/": [
-            {
-                title: 'Introduction',
-                sidebarDepth: 1,    
-                children: [
-                    ["getting-started","Getting Started"]
+        nav: [
+            { 
+                text: 'About',  
+                link: '/',
+                items: [
+                    { text: 'Mission', link: '/' },
+                    { text: 'Who we are', link: '/' },
+                    { text: 'Contact Us', link: '/' },
                 ]
-              }
-          ]
+            },
+            { 
+                text: 'Tracks',  
+                link: '/',
+                items: [
+                    { text: 'Front-End Expert', link: '/' },
+                    { text: 'Back-End Expert', link: '/' },
+                    { text: 'Bootcamp Hero', link: '/' },
+                ]
+            },
+            { 
+                text: 'Languages',  
+                link: '/',
+                items: [
+                    { text: 'English', link: '/' },
+                    { text: 'Portugues', link: '/' },
+                ]
+            },
+            { text: 'GitHub', link: 'https://github.com/alavanQA/alavanqa-docs.github.io' },
+            { text: 'Discord', link: 'https://discord.gg/JUVUDHVh' },
+          ],
+        sidebar: {
+            "/guide/": [{
+                title: 'Introduction',
+                sidebarDepth: 1,
+                children: [
+                    ["getting-started", "Getting Started"]
+                ]
+            }]
         }
-      }
-  }
+    }
+}
