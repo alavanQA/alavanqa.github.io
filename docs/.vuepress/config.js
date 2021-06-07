fd2c6a50d7c3835789c7925ac30d1a68424587cf
Vinicius Fagundes <vinicius_fagundes@epam.com>
2021-06-07 08:20:34 +0200

    aqadocs - navbar - removing default link selection

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,36 +9,70 @@ module.exports = {
     description: 'Documentation Repository for alavanQA material',
     themeConfig: {
         logo: '/assets/logo.png',
-        nav: [
-            { 
-                text: 'About',  
+        nav: [{
+                text: 'About',
                 link: '/',
-                items: [
-                    { text: 'Mission', link: '/' },
-                    { text: 'Who we are', link: '/' },
-                    { text: 'Contact Us', link: '/' },
+                items: [{
+                        text: 'Mission',
+                        link: '',
+                        rel: 'true'
+                    },
+                    {
+                        text: 'Who we are',
+                        link: '',
+                        rel: 'true'
+                    },
+                    {
+                        text: 'Contact Us',
+                        link: '',
+                        rel: 'true'
+                    },
                 ]
             },
-            { 
-                text: 'Tracks',  
+            {
+                text: 'Tracks',
                 link: '/',
-                items: [
-                    { text: 'Front-End Expert', link: '/' },
-                    { text: 'Back-End Expert', link: '/' },
-                    { text: 'Bootcamp Hero', link: '/' },
+                items: [{
+                        text: 'Front-End Expert',
+                        link: '',
+                        rel: 'true'
+                    },
+                    {
+                        text: 'Back-End Expert',
+                        link: '',
+                        rel: 'true'
+                    },
+                    {
+                        text: 'Bootcamp Hero',
+                        link: '',
+                        rel: 'true'
+                    },
                 ]
             },
-            { 
-                text: 'Languages',  
+            {
+                text: 'Languages',
                 link: '/',
-                items: [
-                    { text: 'English', link: '/' },
-                    { text: 'Portugues', link: '/' },
+                items: [{
+                        text: 'English',
+                        link: '',
+                        rel: 'true'
+                    },
+                    {
+                        text: 'Portugues',
+                        link: '',
+                        rel: 'true'
+                    },
                 ]
             },
-            { text: 'GitHub', link: 'https://github.com/alavanQA/alavanqa-docs.github.io' },
-            { text: 'Discord', link: 'https://discord.gg/JUVUDHVh' },
-          ],
+            {
+                text: 'GitHub',
+                link: 'https://github.com/alavanQA/alavanqa-docs.github.io'
+            },
+            {
+                text: 'Discord',
+                link: 'https://discord.gg/JUVUDHVh'
+            },
+        ],
         sidebar: {
             "/guide/": [{
                 title: 'Introduction',
